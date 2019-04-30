36428de88dca351596c760b25e80568f633e022d
Mckay <mckaywrigley@gmail.com>
2019-04-30 15:19:25 -0700

    connects server to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(res => console.log("Connected to MongoDB."))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
